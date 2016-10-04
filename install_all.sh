7a17aacc1dc426c912d3bedf64061a4963f926e7
Jonathan Zhouhan LIN <lin.zhouhan@gmail.com>
2016-10-03 20:14:19 -0400

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -17,28 +17,6 @@ set -e
 set -x
 
 
-if [[ $(arch) != 'x86_64' ]]; then
-    echo "x86_64 required" >&2
-    exit 1
-fi
-
-issue=$(cat /etc/issue)
-extra_packages=
-current=0
-if [[ $issue =~ ^Ubuntu\ 13\.10 ]]; then
-    :
-elif [[ $issue =~ ^Ubuntu\ 14 ]]; then
-    extra_packages=libiberty-dev
-elif [[ $issue =~ ^Ubuntu\ 15\.04 ]]; then
-    extra_packages=libiberty-dev
-elif [[ $issue =~ ^Ubuntu\ 16\.04 ]]; then
-    extra_packages=libiberty-dev
-    current=1
-else
-    echo "Ubuntu 13.10, 14.*, 15.04 or 16.04 required" >&2
-    exit 1
-fi
-
 dir=$(mktemp --tmpdir -d fblualib-build.XXXXXX)
 
 echo Working in $dir
@@ -47,67 +25,22 @@ cd $dir
 
 echo Installing required packages
 echo
-sudo apt-get install -y \
-    git \
-    curl \
-    wget \
-    g++ \
-    automake \
-    autoconf \
-    autoconf-archive \
-    libtool \
-    libboost-all-dev \
-    libevent-dev \
-    libdouble-conversion-dev \
-    libgoogle-glog-dev \
-    libgflags-dev \
-    liblz4-dev \
-    liblzma-dev \
-    libsnappy-dev \
-    make \
-    zlib1g-dev \
-    binutils-dev \
-    libjemalloc-dev \
-    $extra_packages \
-    flex \
-    bison \
-    libkrb5-dev \
-    libsasl2-dev \
-    libnuma-dev \
-    pkg-config \
-    libssl-dev \
-    libedit-dev \
-    libmatio-dev \
-    libpython-dev \
-    libpython3-dev \
-    python-numpy
 
 echo
 echo Cloning repositories
 echo
 if [ $current -eq 1 ]; then
-    git clone --depth 1 https://github.com/facebook/folly
     git clone --depth 1 https://github.com/facebook/fbthrift
     git clone https://github.com/facebook/thpp
     git clone https://github.com/facebook/fblualib
     git clone https://github.com/facebook/wangle
 else
-    git clone -b v0.35.0  --depth 1 https://github.com/facebook/folly
     git clone -b v0.24.0  --depth 1 https://github.com/facebook/fbthrift
     git clone -b v1.0 https://github.com/facebook/thpp
     git clone -b v1.0 https://github.com/facebook/fblualib
 fi
 
-echo
-echo Building folly
-echo
-
-cd $dir/folly/folly
-autoreconf -ivf
-./configure
-make
-sudo make install
-sudo ldconfig # reload the lib paths after freshly installed folly. fbthrift needs it.
+brew install folly
 
 if [ $current -eq 1 ]; then
     echo
